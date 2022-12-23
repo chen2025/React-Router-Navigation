6c322680befbc92fe03e3d0bc74eaf68794a353a
chen2025 <105994099+chen2025@users.noreply.github.com>
2022-12-23 12:41:14 -0500

    Initial commit

---
+++ src/components/Stocks/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Redirect, useHistory } from "react-router-dom";
+
+function Stocks() {
+  let loggedIn = true;
+
+  if (loggedIn === false) {
+    return <Redirect to="/not-logged-in" />;
+  }
+
+  const history = useHistory();
+
+  const handleClick = () => {
+    window.alert("Sending info to the DB!");
+
+    history.push("/");
+  };
+
+  return (
+    <div className="comp orange">
+      <h1>Stocks Component</h1>
+      <button onClick={handleClick}>Home</button>
+    </div>
+  );
+}
+
+export default Stocks;
